9d8504d9902b1313ef221da2c14d3e36c00ce34e
Novsochetra <sochetra12.nov@gmail.com>
2025-08-28 09:53:06 +0700

    chore: adding select option (milliseconds, unix) for unix timestamp converter

--- src/mini-apps/unix-timestamps-converter/screens/home-screen.tsx
+++ src/mini-apps/unix-timestamps-converter/screens/home-screen.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { UTCDate } from "@date-fns/utc";
 
 import { Input } from "@/vendor/shadcn/components/ui/input";
@@ -8,6 +8,19 @@ import { Separator } from "@/vendor/shadcn/components/ui/separator";
 
 import { CurrentUTCDate } from "../components/current-utc-date";
 import { CurrentUnix } from "../components/current-unix";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/vendor/shadcn/components/ui/select";
+import { fromUnixTime } from "date-fns";
+
+enum GenerateMode {
+  Milliseconds = "milliSeconds",
+  Unix = "unix",
+}
 
 const UnixTimeStapConverterScreen = () => {
   return (
@@ -36,35 +49,67 @@ const UnixTimeStapConverterScreen = () => {
 };
 
 const FormConverter = () => {
-  const [milliSeconds, setMilliSeconds] = useState<number>();
+  const [value, setValue] = useState<number>();
+  const [mode, setMode] = useState<GenerateMode>(GenerateMode.Milliseconds);
+  const { utcDateTime, localDateTime } = useMemo(() => {
+    return convertToDates(value, mode);
+  }, [mode, value]);
 
   return (
     <div>
-      <Label className="my-4" htmlFor="millisecondsInput">
-        Convert Milliseconds
-      </Label>
-      <Input
-        id="millisecondsInput"
-        onChange={(e) => {
-          const value = e.target.value;
-          if (!Number.isNaN(value)) {
-            setMilliSeconds(Number(value));
-          }
-        }}
-        autoFocus
-      />
+      <div className="flex flex-1 items-end">
+        <div className="flex flex-1 flex-col pr-4">
+          <Label className="my-4" htmlFor="millisecondsInput">
+            Convert Milliseconds
+          </Label>
+          <Input
+            id="millisecondsInput"
+            onChange={(e) => {
+              const value = e.target.value;
+              if (!Number.isNaN(value)) {
+                setValue(Number(value));
+              }
+            }}
+            autoFocus
+          />
+        </div>
+        <div>
+          <Select
+            defaultValue={GenerateMode.Milliseconds}
+            onValueChange={(v: GenerateMode) => {
+              setMode(v);
+            }}
+          >
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value={GenerateMode.Milliseconds}>
+                Milliseconds
+              </SelectItem>
+              <SelectItem value={GenerateMode.Unix}>Unix</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
       <Label className="my-4">To UTC Date & Time</Label>
-      <Input
-        value={milliSeconds ? new UTCDate(milliSeconds).toString() : ""}
-        readOnly
-      />
+      <Input value={utcDateTime} readOnly />
       <Label className="my-4">To Local Date & Time</Label>
-      <Input
-        value={milliSeconds ? new Date(milliSeconds).toString() : ""}
-        readOnly
-      />
+      <Input value={localDateTime} readOnly />
     </div>
   );
 };
+
+function convertToDates(value: number | undefined, mode: GenerateMode) {
+  if (!value || !mode) return { utc: "", local: "" };
+
+  const date =
+    mode === GenerateMode.Milliseconds ? new Date(value) : fromUnixTime(value);
+
+  return {
+    utcDateTime: new UTCDate(date).toString(),
+    localDateTime: date.toString(),
+  };
+}
 
 export default UnixTimeStapConverterScreen;
