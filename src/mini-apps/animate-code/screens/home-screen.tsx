3e812b41ebf61b67b9cdd355965f17c5b30f213e
Sochetra Nov <sochetra.nov@wingbank.com.kh>
2026-01-02 16:02:09 +0700

    chore(animate-code): using new toolbar style

--- src/mini-apps/animate-code/screens/home-screen.tsx
+++ src/mini-apps/animate-code/screens/home-screen.tsx
@@ -1,14 +1,40 @@
 import { useEffect, useState, useTransition } from "react";
 import { AnimatePresence } from "framer-motion";
+import { useNavigate } from "react-router";
+import { useHotkeys } from "react-hotkeys-hook";
 
 import { AnimatedPage } from "@/vendor/components/animate-page";
-
-import { APP_ID } from "../utils/constants";
 import { defaultPersistEngine } from "@/vendor/zustand/persist";
+import { useAppStore } from "@/main-app/state";
+
 import { ListProject } from "./components/list-project";
+import { AnimateCodeLeftToolbar } from "./components/toolbar/left-toolbar";
+import { APP_ID } from "../utils/constants";
 import { animateCodePersistEngine } from "../state/state";
 
 export const AnimateCodeHomeScreen = () => {
+  const setRightMenubar = useAppStore((state) => state.setRightMenubar);
+  const setLeftMenubar = useAppStore((state) => state.setLeftMenubar);
+  const navigate = useNavigate();
+
+  useHotkeys(
+    "Escape",
+    () => {
+      navigate("/");
+    },
+    { enableOnFormTags: true }
+  );
+
+  useEffect(() => {
+    setRightMenubar(null);
+    setLeftMenubar(<AnimateCodeLeftToolbar />);
+
+    return () => {
+      setRightMenubar(null);
+      setLeftMenubar(null);
+    };
+  }, []);
+  
   return (
     <div className="flex flex-1 min-h-0 overflow-auto">
       <AnimatePresence mode="wait">
